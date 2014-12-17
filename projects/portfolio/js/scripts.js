483dcabb6799f64ecbeb7d9af6bfbc6daee37f90
Arek Mytych <amytych@gmail.com>
2014-12-17 16:05:30 +0100

    Ongoing changes for the front page

--- projects/portfolio/js/scripts.js
+++ projects/portfolio/js/scripts.js
@@ -1,9 +1,65 @@
 $(function() {
+  'use strict';
+
+  var $slides = $('#slides'),
+    $slideTooltipToggler = $('.slide-tooltip-toggler');
+
 
   // Initialize intro slides
-  $('#slides').superslides({
-    inherit_width_from: '.wrapper',
-    pagination: false
+  $slides
+    .superslides({
+      inherit_width_from: '.wrapper',
+      pagination: false
+    })
+    .on('animated.slides', function () {
+      // update slide tooltip index
+      $(this).find('.slides-container > li').each(function (index, el) {
+        if ($(el).css('display') === 'block') {
+          $slideTooltipToggler.data('dialog', 'slide-tooltip-' + index);
+          return false;
+        }
+      });
+    })
+    .on('animating.slides', function () {
+      // Hide the tooltip before animatin the slides
+      $slideTooltipToggler.removeClass('icon-close toggler-active').addClass('icon-circle');
+      $('.slide-tooltip').addClass('visually-hidden').css('opacity', 0);
+    });
+
+
+  // Dialogs (navigation, slide tooltips)
+  $('.dialog-toggler').on('click', function (event) {
+    var $toggler = $(this),
+      dialogId = $toggler.data('dialog'),
+      $dialog = $('#' + dialogId);
+
+    if ($toggler.hasClass('toggler-active')) {
+      $toggler.removeClass('toggler-active');
+      $dialog.css('opacity', 0);
+      setTimeout(function () {
+        $dialog.addClass('visually-hidden');
+      }, 150);
+    } else {
+      $toggler.addClass('toggler-active');
+      $dialog.removeClass('visually-hidden').css('opacity', 1);
+    }
+
+    event.preventDefault();
   });
+
+
+  // Update slide tooltip toggler class
+  $slideTooltipToggler.on('click', function (event) {
+    var $toggler = $(this);
+
+    if ($toggler.hasClass('toggler-active')) {
+      $toggler.removeClass('icon-circle').addClass('icon-close');
+    } else {
+      $toggler.removeClass('icon-close').addClass('icon-circle');
+    }
+
+    event.preventDefault();
+  });
+
 
 });
